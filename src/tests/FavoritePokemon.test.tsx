3ca1899be302372e2ab1389bb31f81aaeed5ced0
Daniel da Silva Ferreira <daniel18mtm@gmail.com>
2024-02-21 21:33:27 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,26 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente FavoritePokemon', () => {
+  test('Testa se não há pokemons favoritos', () => {
+    renderWithRouter(<App />, { route: '/favorites' });
+    expect(screen.getByText(/Pokémon found/i)).toBeInTheDocument();
+  });
+
+  test('Testa se há pokemons favoritos', async () => {
+    const { user } = renderWithRouter(<App />);
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+
+    await user.click(screen.getByRole('link', { name: /more details/i }));
+
+    const favorite = screen.getByRole('checkbox', { name: /favoritado/i }) as HTMLInputElement;
+    expect(favorite.checked).toBe(false);
+    await user.click(favorite);
+    expect(favorite.checked).toBe(true);
+
+    await user.click(screen.getByRole('link', { name: /favorite pokémon/i }));
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+  });
+});
+
