6da6fc98ceb0f0ea4c13e5d583e67f3d4bf9a924
Daniel da Silva Ferreira <daniel18mtm@gmail.com>
2024-02-21 21:19:12 -0300

    Requisito 1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,3 +1,10 @@
-test('', () => {
-    
+import { screen } from '@testing-library/react';
+import renderWithRouter  from '../renderWithRouter';
+import App from '../App';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  renderWithRouter(<App />);
+  expect(screen.getByText(/home/i)).toBeInTheDocument();
+  expect(screen.getByText(/about/i)).toBeInTheDocument();
+  expect(screen.getByText(/favorite pokémon/i)).toBeInTheDocument();
 });
